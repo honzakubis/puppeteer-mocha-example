a46f683673529e4d4ae68a06a826037d20d45215
Jan Kubiš <jankubis@Jan-MacBook-Pro.local>
2020-07-14 19:11:50 +0200

    update prettier config

--- src/launcher.js
+++ src/launcher.js
@@ -1,106 +1,106 @@
 import puppeteer from 'puppeteer'
 
 export default class Launcher {
-	static async build(viewport) {
-		const launchOptions = {
-			headless: true,
-			slowMo: 0,
-			args: [
-				'--no-sandbox',
-				'--disable-setui-sandbox',
-				'--disable-web-security',
-			],
-		}
+  static async build(viewport) {
+    const launchOptions = {
+      headless: true,
+      slowMo: 0,
+      args: [
+        '--no-sandbox',
+        '--disable-setui-sandbox',
+        '--disable-web-security',
+      ],
+    }
 
-		const browser = await puppeteer.launch(launchOptions)
-		const page = await browser.newPage()
-		const extendedPage = new Launcher(page)
-		page.setDefaultTimeout(10000)
+    const browser = await puppeteer.launch(launchOptions)
+    const page = await browser.newPage()
+    const extendedPage = new Launcher(page)
+    page.setDefaultTimeout(10000)
 
-		switch (viewport) {
-			case 'Mobile':
-				const mobileViewport = puppeteer.devices['iPhone X']
-				await page.emulate(mobileViewport)
-				break
-			case 'Tablet':
-				const tabletViewport = puppeteer.devices['iPad landscape']
-				await page.emulate(tabletViewport)
-				break
-			case 'Desktop':
-				await page.setViewport({ width: 1024, height: 768 })
-				break
-			default:
-				throw new Error('Supported devices are only MOBILE | TABLET | DESKTOP')
-		}
+    switch (viewport) {
+      case 'Mobile':
+        const mobileViewport = puppeteer.devices['iPhone X']
+        await page.emulate(mobileViewport)
+        break
+      case 'Tablet':
+        const tabletViewport = puppeteer.devices['iPad landscape']
+        await page.emulate(tabletViewport)
+        break
+      case 'Desktop':
+        await page.setViewport({ width: 1024, height: 768 })
+        break
+      default:
+        throw new Error('Supported devices are only MOBILE | TABLET | DESKTOP')
+    }
 
-		return new Proxy(extendedPage, {
-			get: function (_target, property) {
-				return extendedPage[property] || browser[property] || page[property]
-			},
-		})
-	}
+    return new Proxy(extendedPage, {
+      get: function (_target, property) {
+        return extendedPage[property] || browser[property] || page[property]
+      },
+    })
+  }
 
-	constructor(page) {
-		this.page = page
-	}
+  constructor(page) {
+    this.page = page
+  }
 
-	async getText(selector) {
-		await this.page.waitForSelector(selector)
-		return this.page.$eval(selector, (e) => e.innerHTML)
-	}
+  async getText(selector) {
+    await this.page.waitForSelector(selector)
+    return this.page.$eval(selector, (e) => e.innerHTML)
+  }
 
-	async getCount(selector) {
-		await this.page.waitForSelector(selector)
-		return this.page.$$eval(selector, (items) => items.length)
-	}
+  async getCount(selector) {
+    await this.page.waitForSelector(selector)
+    return this.page.$$eval(selector, (items) => items.length)
+  }
 
-	async waitForText(selector, text) {
-		await this.page.waitForSelector(selector)
-		try {
-			await this.page.waitForFunction(
-				(selector, text) =>
-					document
-						.querySelector(selector)
+  async waitForText(selector, text) {
+    await this.page.waitForSelector(selector)
+    try {
+      await this.page.waitForFunction(
+        (selector, text) =>
+          document
+            .querySelector(selector)
 
-						.innerText.replace(/\s/g, '')
-						.toLowerCase()
-						.includes(text.replace(/\s/g, '').toLowerCase()),
-				{},
-				selector,
-				text
-			)
-		} catch (error) {
-			if (error instanceof puppeteer.errors.TimeoutError) {
-				throw new Error(`Text "${text}" not found for selector "${selector}"`)
-			}
-			throw new Error(error)
-		}
-	}
+            .innerText.replace(/\s/g, '')
+            .toLowerCase()
+            .includes(text.replace(/\s/g, '').toLowerCase()),
+        {},
+        selector,
+        text
+      )
+    } catch (error) {
+      if (error instanceof puppeteer.errors.TimeoutError) {
+        throw new Error(`Text "${text}" not found for selector "${selector}"`)
+      }
+      throw new Error(error)
+    }
+  }
 
-	async waitForXPathAndClick(xpath) {
-		await this.page.waitForXPath(xpath)
-		const elements = await this.page.$x(xpath)
-		if (elements.length > 1) {
-			console.warn('waitForXPathAndClick returned more than one result')
-		}
-		await elements[0].click()
-	}
+  async waitForXPathAndClick(xpath) {
+    await this.page.waitForXPath(xpath)
+    const elements = await this.page.$x(xpath)
+    if (elements.length > 1) {
+      console.warn('waitForXPathAndClick returned more than one result')
+    }
+    await elements[0].click()
+  }
 
-	async autoScrollToBottomOfPage() {
-		await this.page.evaluate(async () => {
-			await new Promise((resolve) => {
-				let totalHeight = 0
-				const distance = 100
-				const timer = setInterval(() => {
-					const scrollHeight = document.body.scrollHeight
-					window.scrollBy(0, distance)
-					totalHeight += distance
-					if (totalHeight >= scrollHeight) {
-						clearInterval(timer)
-						resolve()
-					}
-				}, 200)
-			})
-		})
-	}
+  async autoScrollToBottomOfPage() {
+    await this.page.evaluate(async () => {
+      await new Promise((resolve) => {
+        let totalHeight = 0
+        const distance = 100
+        const timer = setInterval(() => {
+          const scrollHeight = document.body.scrollHeight
+          window.scrollBy(0, distance)
+          totalHeight += distance
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer)
+            resolve()
+          }
+        }, 200)
+      })
+    })
+  }
 }
--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -2,51 +2,51 @@ import { expect } from 'chai'
 import Launcher from '../launcher'
 
 describe('Calculator Test', () => {
-	let page
+  let page
 
-	before(async function () {
-		page = await Launcher.build('Desktop')
-	})
+  before(async function () {
+    page = await Launcher.build('Desktop')
+  })
 
-	after(async function () {
-		await page.close()
-	})
+  after(async function () {
+    await page.close()
+  })
 
-	step('should load calculator website', async function () {
-		await page.goto('https://furbo.sk/playground/kalkulacka.php')
-		await page.waitForText('h1', 'Kalkulačka')
-	})
+  step('should load calculator website', async function () {
+    await page.goto('https://furbo.sk/playground/kalkulacka.php')
+    await page.waitForText('h1', 'Kalkulačka')
+  })
 
-	step('should add numbers', async function () {
-		await page.waitForSelector('#firstInput')
-		await page.type('#firstInput', '10')
+  step('should add numbers', async function () {
+    await page.waitForSelector('#firstInput')
+    await page.type('#firstInput', '10')
 
-		await page.waitForSelector('#secondInput')
-		await page.type('#secondInput', '10')
+    await page.waitForSelector('#secondInput')
+    await page.type('#secondInput', '10')
 
-		await page.waitForSelector('#count')
-		await page.click('#count')
+    await page.waitForSelector('#count')
+    await page.click('#count')
 
-		let result = await page.getText('#result')
-		expect(result).to.equal('20')
-	})
+    let result = await page.getText('#result')
+    expect(result).to.equal('20')
+  })
 
-	step('should deduct numbers', async function () {
-		await page.reload()
-		await page.waitForSelector('#firstInput')
-		await page.type('#firstInput', '10')
+  step('should deduct numbers', async function () {
+    await page.reload()
+    await page.waitForSelector('#firstInput')
+    await page.type('#firstInput', '10')
 
-		await page.waitForSelector('#secondInput')
-		await page.type('#secondInput', '10')
+    await page.waitForSelector('#secondInput')
+    await page.type('#secondInput', '10')
 
-		await page.waitForSelector('#count')
-		await page.click('#deduct')
+    await page.waitForSelector('#count')
+    await page.click('#deduct')
 
-		let result = await page.getText('#result')
-		expect(result).to.equal('0')
-	})
+    let result = await page.getText('#result')
+    expect(result).to.equal('0')
+  })
 
-	step('should reset calculator', async function () {
-		// TODO
-	})
+  step('should reset calculator', async function () {
+    // TODO
+  })
 })
